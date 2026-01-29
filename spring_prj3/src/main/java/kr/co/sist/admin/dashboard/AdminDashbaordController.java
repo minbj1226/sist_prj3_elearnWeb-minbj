bb79ecad7f53214c980c5c6153b2cbe38a553b26
minny2000 <minbj1226@naver.com>
2026-01-30 00:40:43 +0900

    2026-01-30 12:40 url 경로 변경 및 사이드바 수정

--- spring_prj3/src/main/java/kr/co/sist/admin/dashboard/AdminDashbaordController.java
+++ spring_prj3/src/main/java/kr/co/sist/admin/dashboard/AdminDashbaordController.java
@@ -2,11 +2,13 @@ package kr.co.sist.admin.dashboard;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminDashbaordController {
 
-	@GetMapping("/")
+	@GetMapping("/dashboard")
 	public String dashboard() {
 		
 		return "admin/dashboard/dashboard";
